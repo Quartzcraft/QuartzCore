62add8c04237451b0eb752842a60eb0963cde213
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-2.local>
2013-10-07 20:22:45 +1000

    Something goes here ;)

--- src/uk/co/quartzcraft/command/QuartzCommand.java
+++ src/uk/co/quartzcraft/command/QuartzCommand.java
@@ -1,11 +1,5 @@
 package uk.co.quartzcraft.command;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
 public interface QuartzCommand {
 
-	void DefineCommand();
-	
-	boolean RunCommand(CommandSender sender, Command command);
 }
