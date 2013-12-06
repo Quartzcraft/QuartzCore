c1fe88c6483521112911af8399e4a654f0ac5087
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-3.local>
2013-12-06 15:47:42 +1000

    Working on report command

---
+++ src/uk/co/quartzcraft/command/CommandReport.java
@@ -0,0 +1,30 @@
+package uk.co.quartzcraft.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandReport implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] arg0) {
+		
+		Player player = (Player) sender;
+		
+		if(command.getName().equalsIgnoreCase("report")) {
+			if(!(player instanceof Player)){
+	    		player.sendMessage(ChatColor.RED + "This command is for players only!");
+	    	} else {
+	    		if(arg0.length == 0) {
+	    			player.sendMessage(ChatColor.RED + "Please specify a player to report");
+	    		} else {
+	    			
+	    		}
+	    	}
+		} 
+		return false;
+	}
+
+}
