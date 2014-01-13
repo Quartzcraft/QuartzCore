883e93765841ff8e37c478e2a815c26afd42fd90
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-13 13:49:41 +1000

    Connection Listener working now.

--- src/uk/co/quartzcraft/core/listeners/ConnectionListener.java
+++ src/uk/co/quartzcraft/core/listeners/ConnectionListener.java
@@ -1,26 +1,27 @@
 package uk.co.quartzcraft.core.listeners;
 
 import uk.co.quartzcraft.*;
+import uk.co.quartzcraft.core.QuartzCore;
+import uk.co.quartzcraft.core.entity.QPlayer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionListener implements Listener {
-
-	//public String quartzJoinMessage = ChatColor.GOLD + "A player joined!";
-
-	public void setJoinMessage(String joinMessage) {
-		//this.quartzJoinMessage  = joinMessage;
-	}
 	
-	@EventHandler
+	public ConnectionListener(QuartzCore plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+	
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerLogin(PlayerLoginEvent login) {
 		Player player = login.getPlayer();
 		
@@ -31,16 +32,17 @@ public class ConnectionListener implements Listener {
 		
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerJoin(PlayerJoinEvent join) {
 		Player player = join.getPlayer();
 		
 		player.sendMessage("Welcome back, " + player + "!");
-		player.sendMessage("Your QC balance is: ");
+		//player.sendMessage("Your QC balance is: ");
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerQuit(PlayerQuitEvent quit) {
+		//Shouldn't really be monitor, but need to make sure all other permissions plugins are finished.
 		Player player = quit.getPlayer();
 		
 		Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "-" + player + " disconnected!");
