53dc485b6d6481e46b1ec694c51304340485ff5e
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-14 16:42:38 +1000

    Created new QPlayer methods including create player.

--- src/uk/co/quartzcraft/core/entity/QPlayer.java
+++ src/uk/co/quartzcraft/core/entity/QPlayer.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,9 +21,30 @@ public abstract class QPlayer {
 		player.sendMessage(message);
 	}
 	
-	public static ResultSet getData(QPlayer player) {
+	public static ResultSet getData(UUID UUID) {
 		
-		String playername = player.toString();
+		String SUUID = UUID.toString();
+		try {
+			Statement s = QuartzCore.MySQLcore.openConnection().createStatement();
+	        ResultSet res = s.executeQuery("SELECT * FROM PlayerData WHERE UUID ='" + SUUID + "';");
+	        res.next();
+	        
+	        if(res.getString("UUID") == SUUID) {
+	        	return res;
+	        } else {
+	        	return null;
+	        }
+	        
+		} catch(SQLException e) {
+			Logger.getLogger("Minecraft").log(Level.SEVERE, null, e);
+			return null;
+		}
+		
+	}
+	
+	public static ResultSet getQPlayer(QPlayer qplayer) {
+		
+		String playername = qplayer.toString();
 		try {
 			Statement s = QuartzCore.MySQLcore.openConnection().createStatement();
 	        ResultSet res = s.executeQuery("SELECT * FROM PlayerData WHERE DisplayName ='" + playername + "';");
@@ -39,5 +61,57 @@ public abstract class QPlayer {
 		
 	}
 	
+	public static void setConnectionStatus(Player player, boolean conn) {
+		
+	}
+	
+	public static boolean createPlayer(Player player) {
+		
+		long time = System.currentTimeMillis();
+		java.sql.Date date = new java.sql.Date(time);
+		
+		UUID UUID = player.getUniqueId();
+		String SUUID = UUID.toString();
+		
+		try {
+			Statement s = QuartzCore.MySQLcore.openConnection().createStatement();
+			ResultSet res = s.executeQuery("INSERT INTO PlayerData (UUID, DisplayName, JoinDate, PrimaryGroupID, PassedTutorial) VALUES ('" + SUUID +"', '" + player.getDisplayName() + "', " + date + ", 9, 1);");
+	        res.next();
+	        
+	        if(res != null) {
+	        	return true;
+	        } else {
+	        	return false;
+	        }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
 	public abstract HashMap getDataThisPlugin();
+	
+	public abstract boolean createPlayerThisPlugin();
+
+	public static void addUUID(Player player) {
+		UUID UUID = player.getUniqueId();
+		String SUUID = UUID.toString();
+		
+		String playername = player.getDisplayName();
+		
+		try {
+			Statement s = QuartzCore.MySQLcore.openConnection().createStatement();
+	        ResultSet res = s.executeQuery("UPDATE PlayerData SET UUID='" + SUUID + "' WHERE DisplayName='" + playername + "';");
+			res.next();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		
+	}
+
+
 }
