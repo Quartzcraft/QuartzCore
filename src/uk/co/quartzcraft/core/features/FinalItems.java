df97290cb72ffebc9665505e340a3e12d756a534
ZeWildGuy <njb3112@iCloud.com>
2014-05-28 00:04:14 +1000

    Final/Unbreaking changes

--- src/uk/co/quartzcraft/core/features/FinalItems.java
+++ src/uk/co/quartzcraft/core/features/FinalItems.java
@@ -1,13 +1,26 @@
 package uk.co.quartzcraft.core.features;
 
+import org.bukkit.inventory.ItemStack;
 import uk.co.quartzcraft.core.util.ItemUtil;
 
 public class FinalItems {
+    public static final String FINAL_KEY = "__final";
 
-    public static boolean makeFinal(ItemUtil.LoreBuilder item) {
-
-        return true;
+    public static ItemStack makeFinal(ItemStack item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        return makeFinal(ItemUtil.buildLore(item)).save();
+    }
+    public static ItemUtil.LoreBuilder makeFinal(ItemUtil.LoreBuilder builder) {
+        return builder
+                .add(0, "&6&l&nFinal&f (&oCan not be modified)")
+                .setMeta(FINAL_KEY, "1");
     }
 
-    //TODO: Do This!
+    public static boolean isFinal(ItemStack item) {
+        return ItemUtil.hasMeta(item, FINAL_KEY);
+    }
+
+    //TODO: finish This!
 }
--- src/uk/co/quartzcraft/core/features/UnbreakableItems.java
+++ src/uk/co/quartzcraft/core/features/UnbreakableItems.java
@@ -1,12 +1,26 @@
 package uk.co.quartzcraft.core.features;
 
+import org.bukkit.inventory.ItemStack;
 import uk.co.quartzcraft.core.util.ItemUtil;
 
 public class UnbreakableItems {
-    //TODO: Do this!
+    public static final String UNBREAKING_KEY = "__unbreak";
 
-    public static boolean makeUnbreakable(ItemUtil.LoreBuilder item) {
-
-        return true;
+    public static ItemStack makeUnbreaking(ItemStack item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        return makeUnbreaking(ItemUtil.buildLore(item)).save();
     }
+    public static ItemUtil.LoreBuilder makeUnbreaking(ItemUtil.LoreBuilder builder) {
+        return builder
+                .add(0, "&6&l&nUnbreaking&f (&oCan not be damaged)")
+                .setMeta(UNBREAKING_KEY, "1");
+    }
+
+    public static boolean isUnbreakable(ItemStack item) {
+        return ItemUtil.hasMeta(item, UNBREAKING_KEY);
+    }
+
+    //TODO: Finish this
 }
