197f543f79eeecebe2c1009a57d57f7cf6fe8ab8
ZeWildGuy <njb3112@iCloud.com>
2014-05-27 23:52:06 +1000

    wut.

--- src/uk/co/quartzcraft/core/command/CommandPromo.java
+++ src/uk/co/quartzcraft/core/command/CommandPromo.java
@@ -1,21 +1,42 @@
 package uk.co.quartzcraft.core.command;
 
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
+import org.bukkit.*;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import uk.co.quartzcraft.core.QuartzCore;
+import uk.co.quartzcraft.core.chat.ChatPhrase;
+import uk.co.quartzcraft.core.command.framework.*;
+import uk.co.quartzcraft.core.util.InventoryUI;
 
-public class CommandPromo implements CommandExecutor {
+public class CommandPromo {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] arg0) {
-		if(cmd.getName().equalsIgnoreCase("promo")){ 
-			return true;
-		} else {
-			return false;
-		}
-	}
+    private static QuartzCore plugin;
+    private static QCommandFramework framework;
+
+    public CommandPromo(QuartzCore plugin) {
+        this.plugin = plugin;
+        framework = new QCommandFramework(this.plugin);
+        framework.registerCommands(this);
+    }
+
+    InventoryUI menu = new InventoryUI("Current Promotions", 9, new InventoryUI.OptionClickEventHandler() {
+        @Override
+        public void onOptionClick(InventoryUI.OptionClickEvent event) {
+            event.getPlayer().sendMessage("You have claimed the " + event.getName() + " promo!");
+            event.setWillClose(true);
+        }
+    }, plugin)
+            .setOption(3, new ItemStack(Material.APPLE, 1), "Food", "The food is delicious")
+            .setOption(4, new ItemStack(Material.IRON_SWORD, 1), "Weapon", "Weapons are for awesome people")
+            .setOption(5, new ItemStack(Material.EMERALD, 1), "Money", "Money brings happiness");
+
+    @uk.co.quartzcraft.core.command.framework.QCommand(name = "promo", permission = "QCC.promo", description = "Lists the avaliable promos.", usage = "Use /promo")
+    public void promo(CommandArgs args) {
+        Player player = (Player) args.getSender();
+        World world = player.getWorld();
+        player.sendMessage(ChatPhrase.getPhrase("you_are_currently_in_world") + ChatColor.WHITE + world.getName());
+    }
 
 }
