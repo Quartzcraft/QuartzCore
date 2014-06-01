a6ccd270831783bbbe0b18374aac26dd4fdb05d5
ZeWildGuy <njb3112@iCloud.com>
2014-06-02 01:22:05 +1000

    test promo item

--- src/uk/co/quartzcraft/core/features/PromoItems.java
+++ src/uk/co/quartzcraft/core/features/PromoItems.java
@@ -1,5 +1,20 @@
 package uk.co.quartzcraft.core.features;
 
-public class PromoItems {
-    
+import uk.co.quartzcraft.core.util.ItemUtil;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PromoItems extends ItemStack {
+
+    public static final ItemStack MAGIC_DIRT = new ItemStack(Material.DIRT) {{
+        ItemUtil.buildLore(this).setName("&aMagic Dirt")
+                .add("&bThis is a magical item")
+                .add("&bdesigned to test /promo").save();
+    }};
 }
