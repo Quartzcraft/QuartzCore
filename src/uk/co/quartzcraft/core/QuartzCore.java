6d0beec7d10b6948145e8eb3abc09d8119cabd4d
ZeWildGuy <njb3112@iCloud.com>
2014-06-05 06:36:52 +1000

    CommandReport register

--- src/uk/co/quartzcraft/core/QuartzCore.java
+++ src/uk/co/quartzcraft/core/QuartzCore.java
@@ -121,9 +121,9 @@ public class QuartzCore extends JavaPlugin {
         commandFramework.registerCommands(new CommandQCReload(this));
         commandFramework.registerCommands(new CommandAnnounce(this));
         commandFramework.registerCommands(new CommandPromo(this));
+        commandFramework.registerCommands(new CommandReport(this));
 	   	getCommand("quartz").setExecutor(new CommandQuartz());
 	   	getCommand("m").setExecutor(new CommandM());
-	   	getCommand("report").setExecutor(new CommandReport());
 	   	getCommand("register").setExecutor(new CommandRegister());
 	   	
 	   	//Database
