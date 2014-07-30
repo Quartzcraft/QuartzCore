0382e9d750c9bb8ef459b7e081a26a16dad3a06c
ZeWildGuy <njb3112@iCloud.com>
2014-07-30 20:30:19 +1000

    Extra utils

--- src/uk/co/quartzcraft/core/util/Util.java
+++ src/uk/co/quartzcraft/core/util/Util.java
@@ -1,12 +1,22 @@
 package uk.co.quartzcraft.core.util;
 
+import java.util.Date;
+import java.sql.Timestamp;
+
 public class Util {
 
-    public static void printException(Exception e) {
+    public static void printException(String message, Exception e) {
 
     }
 
     public static String colour(String s) {
         return ChatUtil.parse(s);
+    }
+
+    public static long timestamp() {
+        java.util.Date date= new java.util.Date();
+        Timestamp ts1 = new Timestamp(date.getTime());
+        long tsTime1 = ts1.getTime();
+        return tsTime1;
     }
 }
