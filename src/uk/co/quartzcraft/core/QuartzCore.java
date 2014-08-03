156009014bebd30b610325cd094a2c1a0abf8dfa
ZeWildGuy <njb3112@iCloud.com>
2014-08-03 16:15:56 +1000

    Blank line

--- src/uk/co/quartzcraft/core/QuartzCore.java
+++ src/uk/co/quartzcraft/core/QuartzCore.java
@@ -127,6 +127,7 @@ public class QuartzCore extends JavaPlugin {
         QCChat.addPhrase("please_specify_player_to_report", "&cYou must specify a player to report!");
         QCChat.addPhrase("thank_you_for_reporting_user", "&aThank you for submitting a report. It is unlikely that we will need contact you.");
         QCChat.addPhrase("error_submitting_report", "&cAn error occurred while submitting your report! Please try again or contact a QuartzCraft administrator.");
+
         QCChat.addPhrase("you_are_currently_in_world", "&aYou are currently in world ");
 		
 		QCChat.addPhrase("Server_Full", "&cServer Full!\n &aIt appears that the server is full. Please try again later, or purchase a supporter rank at http://quartzcraft.co.uk/index.php?upgrade");
