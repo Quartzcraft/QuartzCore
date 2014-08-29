f52b9befa20e0c2fa60c7724a113ff599b18daea
ZeWildGuy <njb3112@iCloud.com>
2014-08-29 16:43:27 +1000

    Created new teleport commands

---
+++ src/uk/co/quartzcraft/core/command/CommandTP.java
@@ -0,0 +1,49 @@
+package uk.co.quartzcraft.core.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import uk.co.quartzcraft.core.QuartzCore;
+import uk.co.quartzcraft.core.command.framework.CommandArgs;
+import uk.co.quartzcraft.core.command.framework.QCommand;
+import uk.co.quartzcraft.core.command.framework.QCommandFramework;
+import uk.co.quartzcraft.core.systems.chat.QCChat;
+
+public class CommandTP {
+
+    private static QuartzCore plugin;
+    private static QCommandFramework framework;
+
+    public CommandTP(QuartzCore plugin) {
+        this.plugin = plugin;
+        framework = new QCommandFramework(this.plugin);
+        framework.registerCommands(this);
+    }
+
+    @QCommand(name = "go", permission = "QCC.tp.go", description = "Teleports you to another player", usage = "/go [player]")
+    public void go(CommandArgs args) {
+        String args0[] = args.getArgs();
+        Player bukkitPlayer = (Player) args.getSender();
+        Player target = Bukkit.getPlayer(args0[0]);
+        if(Bukkit.getServer().getPlayer(args0[0]).isOnline()) {
+            bukkitPlayer.teleport(target);
+            bukkitPlayer.sendMessage(QCChat.getPhrase("teleported_you_to_player_X") + target.getName());
+        } else {
+            bukkitPlayer.sendMessage(QCChat.getPhrase("specify_online_username"));
+        }
+    }
+
+    @QCommand(name = "go", permission = "QCC.tp.get", description = "Teleports the specified player to you", usage = "/get [player]")
+    public void get(CommandArgs args) {
+        String args0[] = args.getArgs();
+        Player bukkitPlayer = (Player) args.getSender();
+        Player target = Bukkit.getPlayer(args0[0]);
+        if(target.isOnline()) {
+            target.teleport(target);
+            target.sendMessage(QCChat.getPhrase("teleported_you_to_player_X") + bukkitPlayer.getName());
+            bukkitPlayer.sendMessage(target.getName() + QCChat.getPhrase("X_has_been_teleported_to_you"));
+        } else {
+            bukkitPlayer.sendMessage(QCChat.getPhrase("specify_online_username"));
+        }
+    }
+}
