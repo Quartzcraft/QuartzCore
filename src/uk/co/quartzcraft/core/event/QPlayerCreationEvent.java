8e8d1a844c350f4f992c6663d094d85ab770b10e
ZeWildGuy <njb3112@iCloud.com>
2014-08-30 10:35:13 +1000

    QPlayer used in QPlayerCreationEvent

--- src/uk/co/quartzcraft/core/event/QPlayerCreationEvent.java
+++ src/uk/co/quartzcraft/core/event/QPlayerCreationEvent.java
@@ -3,17 +3,29 @@ package uk.co.quartzcraft.core.event;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import uk.co.quartzcraft.core.data.QPlayer;
 
 public class QPlayerCreationEvent extends Event {
 	private static final HandlerList handlers = new HandlerList();
     private Player player;
- 
-    public QPlayerCreationEvent(Player arg) {
+    private QPlayer qPlayer;
+
+    public QPlayerCreationEvent(Player arg, QPlayer arg1) {
         player = arg;
+        qPlayer = arg1;
+    }
+
+    public QPlayerCreationEvent(QPlayer arg) {
+        player = arg.getPlayer();
+        qPlayer = arg;
     }
  
     public Player getPlayer() {
         return player;
+    }
+
+    public QPlayer getQPlayer() {
+        return qPlayer;
     }
  
     public HandlerList getHandlers() {
