619f2a792f04eddeb22fad5c411cc3611a3cbfbd
ZeWildGuy <njb3112@iCloud.com>
2014-09-14 07:33:28 +1000

    Using ArrayList's

--- src/uk/co/quartzcraft/core/systems/websync/UpdateGroups.java
+++ src/uk/co/quartzcraft/core/systems/websync/UpdateGroups.java
@@ -10,38 +10,41 @@ import uk.co.quartzcraft.core.util.Util;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UpdateGroups {
 
     public static void update() {
         TaskChain.newChain().add(new TaskChain.AsyncFirstTask() {
+            ArrayList<Integer> ids = new ArrayList();
+
             @Override
-            protected QPlayer[] run() {
-                QPlayer[] players = null;
+            protected ArrayList run() {
                 try {
                     ResultSet res = QuartzCore.DBCore.createStatement().executeQuery("SELECT * FROM PlayerData;");
-                    int i = 0;
                     while (res.next()) {
-                        players[i] = new QPlayer(res.getInt("id"));
-                        i++;
+                        ids.add(res.getInt("id"));
                     }
                 } catch (SQLException e) {
                     Util.printException("Failed to retrieve QPlayer from database", e);
                 }
-                return players;
+                return ids;
             }
-        }).add(new TaskChain.AsyncLastTask<QPlayer[]>() {
+        }).add(new TaskChain.AsyncLastTask<ArrayList<Integer>>() {
             @Override
-            protected void run(QPlayer[] players) {
-                for (QPlayer player : players) {
+            protected void run(ArrayList<Integer> ids) {
+                for (int id : ids) {
                     try {
-                        JSONObject jsonObject = JSONMethods.decode(Websync.getUser(player.getName()));
+                        QPlayer player = new QPlayer(id);
+                        if(QuartzCore.plugin.getConfig().getBoolean("settings.websync.active")) {
+                            JSONObject jsonObject = JSONMethods.decode(Websync.getUser(player.getName()));
 
-                        // Getting the value of a attribute in a JSONObject
-                        String groups = jsonObject.getString("secondary_group_ids");
+                            // Getting the value of a attribute in a JSONObject
+                            String groups = jsonObject.getString("secondary_group_ids");
 
-                        String siteGroups = groups;
-                        player.setPrimaryGroup(Groups.findGroupFromSite(siteGroups));
+                            String siteGroups = groups;
+                            player.setPrimaryGroup(Groups.findGroupFromSite(siteGroups));
+                        }
                     } catch(NoValidGroupException e) {
                         Util.printException("No valid groups", e);
                     }
