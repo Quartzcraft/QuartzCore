714d3946f516a3a515053e43bca38bb910197688
ZeWildGuy <njb3112@iCloud.com>
2014-09-28 12:24:08 +1000

    Finished /pinfo command

--- src/uk/co/quartzcraft/core/command/CommandPinfo.java
+++ src/uk/co/quartzcraft/core/command/CommandPinfo.java
@@ -19,18 +19,16 @@ public class CommandPinfo {
         framework.registerCommands(this);
     }
 
-    //TODO Finish/fix up
     @QCommand(name = "pinfo", aliases = { "p" }, permission = "QCC.pinfo", description = "Displays info on the specified player", usage = "Use /pinfo [playername]")
     public void pinfo(CommandArgs args) {
         if(args.getArgs().length >= 1) {
             String[] arg1 = args.getArgs();
-            Player pplayer = Bukkit.getPlayer(arg1[0]);
-            QPlayer player = new QPlayer(pplayer);
+            QPlayer target = new QPlayer(arg1[0]);
 
             args.getSender().sendMessage(QCChat.getPhrase("information_on_player_X") + arg1[0]);
-            args.getSender().sendMessage(QCChat.getPhrase("group") + player.getGroup().getFancyName());
-            args.getSender().sendMessage(QCChat.getPhrase("first_join") + player.getFirstJoin());
-            args.getSender().sendMessage(QCChat.getPhrase("last_seen") + player.getLastSeen());
+            args.getSender().sendMessage(QCChat.getPhrase("group") + target.getGroup().getFancyName());
+            args.getSender().sendMessage(QCChat.getPhrase("first_join") + target.getFirstJoin());
+            args.getSender().sendMessage(QCChat.getPhrase("last_seen") + target.getLastSeen());
         } else {
             args.getSender().sendMessage(QCChat.getPhrase("specify_username"));
         }
