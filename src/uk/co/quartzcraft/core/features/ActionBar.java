5422c0a9a264a0e2fa1a234a07214b1603182baa
ZeWildGuy <njb3112@iCloud.com>
2014-10-02 14:03:15 +1000

    Added action bar support

---
+++ src/uk/co/quartzcraft/core/features/ActionBar.java
@@ -0,0 +1,18 @@
+package uk.co.quartzcraft.core.features;
+
+import net.minecraft.server.v1_7_R4.ChatSerializer;
+import net.minecraft.server.v1_7_R4.IChatBaseComponent;
+import net.minecraft.server.v1_7_R4.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class ActionBar {
+
+    public static void displayBar(Player p, String message) {
+        IChatBaseComponent barmsg = ChatSerializer.a(message);
+        PacketPlayOutChat bar = new PacketPlayOutChat(barmsg, true);
+        //PacketPlayOutChat bar = new PacketPlayOutChat(barmsg, 2);
+
+        ((CraftPlayer) p).getHandle().playerConnection.sendPacket(bar);
+    }
+}
