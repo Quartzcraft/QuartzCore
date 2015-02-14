884ae3d1c007172b8a29625143fa8765fbd04d7d
ZeWildGuy <njb3112@iCloud.com>
2015-02-15 09:49:45 +1000

    Actual alert class

---
+++ src/uk/co/quartzcraft/core/systems/notifications/Alert.java
@@ -0,0 +1,82 @@
+package uk.co.quartzcraft.core.systems.notifications;
+
+import uk.co.quartzcraft.core.QuartzCore;
+import uk.co.quartzcraft.core.data.QPlayer;
+import uk.co.quartzcraft.core.data.Server;
+import uk.co.quartzcraft.core.systems.chat.QCChat;
+import uk.co.quartzcraft.core.util.Util;
+
+import java.util.logging.Level;
+
+public class Alert {
+
+    private String prefix = null;
+    private String message = null;
+    private String[] args = null;
+    private boolean displayPrefix = true;
+    private AlertType alertType = null;
+    private Server server = null;
+    private QPlayer player = null;
+    private boolean read = false;
+
+    public Alert(String pre, String msg, String[] argss, boolean dP, String type, Server s) {
+        if(msg.equals("") && type.equals("") && args == null) {
+            Util.log(Level.WARNING, "Invalid alert arguments!");
+        }
+        this.prefix = pre;
+        this.message = msg;
+        this.args = argss;
+        this.displayPrefix = dP;
+        this.alertType = AlertTypeHandler.getAlertType(type);
+        this.server = s;
+    }
+
+    public Alert(String pre, String msg, String[] argss, boolean dP, String type, Server s, QPlayer receiver) {
+        if(msg.equals("") && type.equals("") && args == null) {
+            Util.log(Level.WARNING, "Invalid alert arguments!");
+        }
+        this.prefix = pre;
+        this.message = msg;
+        this.args = argss;
+        this.displayPrefix = dP;
+        this.alertType = AlertTypeHandler.getAlertType(type);
+        this.server = s;
+        this.player = receiver;
+    }
+
+    public void send() {
+        if(this.player == null) {
+            Util.log(Level.WARNING, "Attempting to send alert with no specified player");
+            return;
+        }
+        this.send(this.player);
+    }
+
+    public void send(QPlayer player) {
+        String Apre = "";
+        String message = "";
+        if(player.getPlayer().hasPermission(alertType.permission())) {
+            if (alertType.requireArgs()) {
+                //TODO execute code from the AlertType
+            } else {
+                message = Apre + alertType.prefix() + this.message;
+            }
+
+            if(displayPrefix) {
+                Apre = QCChat.getPhrase("alert_prefix");
+            }
+
+            if (player.isOnline()) {
+                player.getPlayer().sendMessage(message);
+            }
+        }
+    }
+
+    public void setAsRead() {
+        this.read = true;
+    }
+
+    public void setAsUnRead() {
+        this.read = true;
+    }
+}
