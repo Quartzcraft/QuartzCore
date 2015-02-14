832dbf4670cf65b3ee54baf0e83e7282f32c6e7a
ZeWildGuy <njb3112@iCloud.com>
2015-02-15 09:49:18 +1000

    Alert types @interface

---
+++ src/uk/co/quartzcraft/core/systems/notifications/AlertType.java
@@ -0,0 +1,45 @@
+package uk.co.quartzcraft.core.systems.notifications;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AlertType {
+
+    /**
+     * The name of the alert type.
+     *
+     * @return
+     */
+    public String name();
+
+    /**
+     * Gets the required permission to recieve this type of alert
+     *
+     * @return
+     */
+    public String permission() default "";
+
+    /**
+     * The prefix displayed in front of the alert so that the user receiving knows what plugin or subsystem sent the alert
+     *
+     * @return
+     */
+    public String prefix() default "";
+
+    /**
+     * Determine whether certain arguments must be passed to the alert. If this is set to false, only the message and prefix will be sent.
+     *
+     * @return
+     */
+    public boolean requireArgs() default false;
+
+    /**
+     * A list of the required arguments.
+     * @return
+     */
+    public String[] requiredArgs() default {};
+}
