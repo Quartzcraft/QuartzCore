766a6ea6519c42c70226480c3b081dabe7af563f
ZeWildGuy <njb3112@iCloud.com>
2015-02-21 11:08:23 +1000

    Player data and sessions

--- src/uk/co/quartzcraft/core/data/QServer.java
+++ src/uk/co/quartzcraft/core/data/QServer.java
@@ -1,6 +1,84 @@
 package uk.co.quartzcraft.core.data;
 
+import org.bukkit.entity.Player;
+import uk.co.quartzcraft.core.systems.chat.QCChat;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class QServer {
 
+    private int id;
+    private String name;
+
+    private Map<Integer, QPlayer> playerMap = new HashMap<Integer, QPlayer>();
+
+    private Map<UUID, Integer> playerUUIDMap = new HashMap<UUID, Integer>();
+    private Map<Player, Integer> playerPlayerMap = new HashMap<Player, Integer>();
+    private Map<String, Integer> playerNameMap = new HashMap<String, Integer>();
+
+    public QServer() {
+
+    }
+
+    public void createPlayerSession(Player player) {
+        QPlayer qPlayer = new QPlayer(player);
+
+        if(qPlayer.isOnline()) {
+            player.kickPlayer(QCChat.getPhrase("you_can_only_be_connected_to_one_server_at_a_time"));
+        }
+
+        playerMap.put(qPlayer.getID(), qPlayer);
+        playerUUIDMap.put(qPlayer.getUniqueId(), qPlayer.getID());
+        playerPlayerMap.put(player, qPlayer.getID());
+        playerNameMap.put(player.getName(), qPlayer.getID());
+    }
+
+    public QPlayer getPlayer(UUID uuid) {
+        if(playerUUIDMap.containsKey(uuid)) {
+            return playerMap.get(playerUUIDMap.get(uuid));
+        }
+
+        return new QPlayer(uuid);
+    }
+
+    public QPlayer getPlayer(Player player) {
+        if(playerPlayerMap.containsKey(player)) {
+            return playerMap.get(playerPlayerMap.get(player));
+        }
+
+        return new QPlayer(player);
+    }
+
+    public QPlayer getPlayer(int id) {
+        if(playerMap.containsKey(id)) {
+            return playerMap.get(id);
+        }
+
+        return new QPlayer(id);
+    }
+
+    public QPlayer getPlayer(String name) {
+        if(playerNameMap.containsKey(name)) {
+            return playerMap.get(playerNameMap.get(name));
+        }
+
+        return new QPlayer(name);
+    }
+
+    public void destoryPlayerSession(int id) {
+        if(playerMap.containsKey(id)) {
+            QPlayer qPlayer = playerMap.get(id);
+            qPlayer.save();
+
+            playerUUIDMap.remove(qPlayer.getUniqueId());
+            playerPlayerMap.remove(qPlayer.getPlayer());
+            playerNameMap.remove(qPlayer.getName());
+            playerMap.remove(id);
+
+            qPlayer = null;
+        }
+    }
 
 }
