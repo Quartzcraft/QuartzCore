d477e8acc741cee6150e5003fe4728b8deb77a87
ZeWildGuy <njb3112@iCloud.com>
2015-09-27 21:14:22 +1000

    Actually made AlertArgs do something

--- src/uk/co/quartzcraft/core/systems/notifications/AlertArgs.java
+++ src/uk/co/quartzcraft/core/systems/notifications/AlertArgs.java
@@ -1,8 +1,35 @@
 package uk.co.quartzcraft.core.systems.notifications;
 
+import java.util.HashMap;
+
 public class AlertArgs {
+
+    private HashMap<String, Object> args = new HashMap<String, Object>();
 
     public AlertArgs() {
 
+    }
+
+    public AlertArgs setString(String k, String value) {
+        args.put(k, value);
+        return this;
+    }
+
+    public AlertArgs setInt(String k, int value) {
+        args.put(k, value);
+        return this;
+    }
+
+    public AlertArgs setBoolean(String k, boolean value) {
+        args.put(k, value);
+        return this;
+    }
+
+    public HashMap getArgs() {
+        return this.args;
+    }
+
+    public Object getArg(String key) {
+        return this.args.get(key);
     }
 }
