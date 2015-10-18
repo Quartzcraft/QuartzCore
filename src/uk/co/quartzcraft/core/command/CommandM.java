2918ba57ff975d7a70be0775a5436e0e920958cb
ZeWildGuy <njb3112@iCloud.com>
2015-10-18 21:54:39 +1000

    Migrated CommandM to new system

--- src/uk/co/quartzcraft/core/command/CommandM.java
+++ src/uk/co/quartzcraft/core/command/CommandM.java
@@ -6,40 +6,40 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import uk.co.quartzcraft.core.QuartzCore;
+import uk.co.quartzcraft.core.command.framework.CommandArgs;
+import uk.co.quartzcraft.core.command.framework.QCommand;
+import uk.co.quartzcraft.core.command.framework.QCommandFramework;
 import uk.co.quartzcraft.core.util.Util;
 
-public class CommandM implements CommandExecutor {
+public class CommandM {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(command.getName().equalsIgnoreCase("m")){ 
-			
-			if (!(sender instanceof Player)) {
-				sender.sendMessage(ChatColor.RED + "This command can only be run by a player.");
-				return false;
-			} else {
-				Player player = (Player) sender;
-				Object gameMode = player.getGameMode();
-				
-				if (gameMode == GameMode.SURVIVAL) {
-					player.setGameMode(GameMode.CREATIVE);
-					Util.sendMsg(player, ChatColor.GOLD + "Your gamemode is now " + ChatColor.GREEN + "CREATIVE" + ChatColor.GOLD + "!");
-				} else if (gameMode == GameMode.CREATIVE) {
-					player.setGameMode(GameMode.SURVIVAL);
-					Util.sendMsg(player, ChatColor.GOLD + "Your gamemode is now " + ChatColor.GREEN + "SURVIVAL" + ChatColor.GOLD + "!");
-				} else if (gameMode == GameMode.ADVENTURE) {
-					player.setGameMode(GameMode.SURVIVAL);
-					Util.sendMsg(player, ChatColor.GOLD + "Your gamemode is now " + ChatColor.GREEN + "SURVIVAL" + ChatColor.GOLD + "!");
-				} else {
-					Util.sendMsg(player, ChatColor.RED + "Your gamemode could not be changed!");
-				}
-			}
-			
-			return true;
+	private static QuartzCore plugin;
+	private static QCommandFramework framework;
+
+	public CommandM(QuartzCore plugin) {
+		this.plugin = plugin;
+		framework = new QCommandFramework(this.plugin);
+		framework.registerCommands(this);
+	}
+
+	@QCommand(name = "m", permission = "QCC.m", description = "Quickly changes gamemode", usage = "/m")
+	public void mCommand(CommandArgs args) {
+		Player player = args.getPlayer();
+		Object gameMode = player.getGameMode();
+
+		if (gameMode == GameMode.SURVIVAL) {
+			player.setGameMode(GameMode.CREATIVE);
+			Util.sendMsg(player, ChatColor.GOLD + "Your gamemode is now " + ChatColor.GREEN + "CREATIVE" + ChatColor.GOLD + "!");
+		} else if (gameMode == GameMode.CREATIVE) {
+			player.setGameMode(GameMode.SURVIVAL);
+			Util.sendMsg(player, ChatColor.GOLD + "Your gamemode is now " + ChatColor.GREEN + "SURVIVAL" + ChatColor.GOLD + "!");
+		} else if (gameMode == GameMode.ADVENTURE) {
+			player.setGameMode(GameMode.SURVIVAL);
+			Util.sendMsg(player, ChatColor.GOLD + "Your gamemode is now " + ChatColor.GREEN + "SURVIVAL" + ChatColor.GOLD + "!");
 		} else {
-			return false;
+			Util.sendMsg(player, ChatColor.RED + "Your gamemode could not be changed!");
 		}
-		//sender.sendMessage(ChatColor.RED + "Your gamemode could not be changed!");
 	}
 
 }
