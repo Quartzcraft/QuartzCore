a18e1b750462d78dac30f7a839e08852b5830339
ZeWildGuy <njb3112@iCloud.com>
2015-12-06 16:29:22 +1000

    Imported elixr and items.yml

---
+++ src/uk/co/quartzcraft/core/util/elixr/ChatUtils.java
@@ -0,0 +1,24 @@
+package uk.co.quartzcraft.core.util.elixr;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class ChatUtils {
+    
+    /**
+     * 
+     * @param loc
+     * @param radius
+     * @param msg
+     */
+    public static void notifyNearby( Location loc, int radius, String msg ){
+        for ( final Player p : Bukkit.getServer().getOnlinePlayers() ) {
+            if( loc.getWorld().equals( p.getWorld() ) ) {
+                if( loc.distance( p.getLocation() ) <= radius ) {
+                    p.sendMessage( msg );
+                }
+            }
+        }
+    }
+}
---
+++ src/uk/co/quartzcraft/core/util/elixr/WorldUtils.java
@@ -0,0 +1,18 @@
+package uk.co.quartzcraft.core.util.elixr;
+
+import org.bukkit.Location;
+import org.bukkit.World.Environment;
+
+public class WorldUtils {
+    
+    /**
+     * Creates lightning that doesn't strike the ground, only thunder is heard
+     * @param loc the location to strike lightning above
+     */
+    public static void thunder( Location loc ){
+        loc.setY(350D);
+        if(loc.getWorld().getEnvironment() == Environment.NORMAL){
+            loc.getWorld().strikeLightningEffect(loc);
+        }
+    }
+}
